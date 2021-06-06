9d88912980e5a27bbbbb1426403192302a61ac15
Cole Theodore <theodoco@oregonstate.edu>
2021-06-06 14:52:54 -0700

    added tests

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+# Tests for calc app
+
+import calculator
+
+
+class TestCalculatorApp:
+    def test_add(self):
+        assert 5 == calculator.add(3, 2)
+
+    def test_subtract(self):
+        assert 5 == calculator.subtract(10, 5)
+
+    def test_multiply(self):
+        assert 6 == calculator.multiply(3, 2)
+
+    def test_divide(self):
+        assert 4 == calculator.divide(8, 2)
